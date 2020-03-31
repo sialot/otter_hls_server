1930743f3e89644c93d93f102c5414b8df43a8bc
sialot <sialot@sina.com>
2020-03-31 14:17:51 +0800

    配置文件支持日志级别

--- log/logger.go
+++ log/logger.go
@@ -13,11 +13,23 @@ func InitLog() {
 
 	fileName, _ := config.SysConfig.Get("log.syslog.filename")
 	pattern, _ := config.SysConfig.Get("log.syslog.pattern")
+	level, _ := config.SysConfig.Get("log.syslog.level")
+
+	var l = ezlog.LVL_DEBUG
+	switch level {
+	case "info":
+		l = ezlog.LVL_INFO
+	case "warn":
+		l = ezlog.LVL_WARN
+	case "error":
+		l = ezlog.LVL_ERROR
+	}
 
 	Log = &ezlog.Log{
 		Filename:   fileName,
 		Pattern:    pattern,
-		BufferSize: 0}
+		BufferSize: 0,
+		LogLevel: l}
 }
 
 // FlushLog 清空缓冲区
